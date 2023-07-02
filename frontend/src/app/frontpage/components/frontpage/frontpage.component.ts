d1490a57e2bf20f99e204c66546420f9156afdd1
Usman Muhammad <usman.muhammad@sanoma.com>
2023-07-02 22:10:19 +0300

    add pagincation to equipments list

--- frontend/src/app/frontpage/components/frontpage/frontpage.component.ts
+++ frontend/src/app/frontpage/components/frontpage/frontpage.component.ts
@@ -1,28 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { Router, ActivatedRoute, Data } from '@angular/router';
-import { Equipment } from 'src/app/core/models/equipment';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SearchService } from 'src/app/core/services';
+import { Equipment } from 'src/app/core/models';
 
 @Component({
+  selector: 'app-frontpage',
   templateUrl: './frontpage.component.html',
   styleUrls: ['./frontpage.component.scss']
 })
 export class FrontpageComponent {
   searchNumber: number = 5;
   dataSource: MatTableDataSource<Equipment> = new MatTableDataSource<Equipment>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(private searchService: SearchService, private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  onSubmit(): void {
-    if (!this.searchNumber) {
-      return;
-    }
+  ngOnInit() {
     this.searchService.searchByLimit(this.searchNumber).subscribe(
       (result: Equipment[]) => {
         this.dataSource.data = result;
+        this.dataSource.paginator = this.paginator;
       },
       (error: any) => console.log('Error:', error)
     );
   }
 
+  onPageChange(event: PageEvent) {
+    console.log(event.pageSize)
+    const startIndex = event.pageIndex * event.pageSize;
+    const endIndex = startIndex + event.pageSize;
+
+    this.searchService.searchByLimit(event.pageSize).subscribe(
+      (result: Equipment[]) => {
+        this.dataSource.data = result.slice(startIndex, endIndex);
+      },
+      (error: any) => console.log('Error:', error)
+    );
+  }
 }
