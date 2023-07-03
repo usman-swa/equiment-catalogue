16ca96ab785f564586abbddec4bab1a4f78c815c
Usman Muhammad <usman.muhammad@sanoma.com>
2023-07-03 10:19:37 +0300

    code cleanup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CoreModule } from './core/core.module';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSortModule } from '@angular/material/sort';
-import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
--- frontend/src/app/frontpage/frontpage.module.ts
+++ frontend/src/app/frontpage/frontpage.module.ts
@@ -14,6 +14,7 @@ import { CoreModule } from '../core/core.module';
 import { MatButtonModule } from '@angular/material/button';
 import { AddNewEquipmentComponent, ListEquipmentsComponent } from '../equipment';
 import { FrontpageComponent } from './components';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
     imports: [FrontpageRouting,
@@ -30,7 +31,9 @@ import { FrontpageComponent } from './components';
       MatInputModule,
       MatPaginatorModule,
       MatFormFieldModule,
-      MatSelectModule],
+      MatSelectModule,
+      MatIconModule
+    ],
     declarations: [FrontpageComponent, ListEquipmentsComponent, AddNewEquipmentComponent],
     exports: [FrontpageComponent, ListEquipmentsComponent, AddNewEquipmentComponent],
 })
